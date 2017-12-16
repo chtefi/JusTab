7da35a1d69ad6174589059763d5d1f6da7422fa4
Stéphane Derosiaux <derste@gmail.com>
2017-12-16 14:16:38 +0100

    Show beststories

--- js/background/v-hacker-news.js
+++ js/background/v-hacker-news.js
@@ -1,54 +1,54 @@
 // Docs:
 // https://github.com/HackerNews/API
-import ajax from 'modules/ajax';
+import ajax from 'modules/ajax'
 
 export default {
   computed: {
-    hackerNewsService () {
-      return this.services.find(s => s.id === 7);
+    hackerNewsService() {
+      return this.services.find(s => s.id === 7)
     }
   },
 
   methods: {
-    hackerNews () {
-      localStorage.setItem('hackerNewsError', false);
+    hackerNews() {
+      localStorage.setItem('hackerNewsError', false)
       return this.hackerNewsStoryIds()
         .then(this.hackerNewsStories)
         .then(this.hackerNewsComponents)
-        .catch((error) => {
-          if (error) console.error(error);
-          localStorage.setItem('hackerNewsError', true);
-        });
+        .catch(error => {
+          if (error) console.error(error)
+          localStorage.setItem('hackerNewsError', true)
+        })
     },
 
-    hackerNewsStoryIds () {
+    hackerNewsStoryIds() {
       return new Promise((resolve, reject) => {
-        var url = 'https://hacker-news.firebaseio.com/v0/';
-        var apiCall = 'topstories.json';
+        var url = 'https://hacker-news.firebaseio.com/v0/'
+        var apiCall = 'beststories.json'
 
         ajax('GET', url + apiCall)
           .then(function(data) {
-            resolve(data.slice(0,25));
+            resolve(data.slice(0, 25))
           })
-          .catch(reject);
-      });
+          .catch(reject)
+      })
     },
 
-    hackerNewsStories (ids) {
-      var promises = [];
+    hackerNewsStories(ids) {
+      var promises = []
 
       ids.forEach(function(id) {
-        var url = 'https://hacker-news.firebaseio.com/v0/';
-        var apiCall = `item/${id}.json`;
-        promises.push(ajax('GET', url + apiCall));
-      });
+        var url = 'https://hacker-news.firebaseio.com/v0/'
+        var apiCall = `item/${id}.json`
+        promises.push(ajax('GET', url + apiCall))
+      })
 
-      return Promise.all(promises);
+      return Promise.all(promises)
     },
 
-    hackerNewsComponents (stories) {
-      let components = [];
-      stories.forEach((story) => {
+    hackerNewsComponents(stories) {
+      let components = []
+      stories.forEach(story => {
         components.push({
           name: 'v-panel-item',
           props: {
@@ -57,10 +57,10 @@ export default {
             subtitleUrl: `https://news.ycombinator.com/item?id=${story.id}`,
             subtitle: `${story.descendants} comments - ${story.score} points`
           }
-        });
-      });
+        })
+      })
 
-      localStorage.setItem('hackerNewsComponents', JSON.stringify(components));
+      localStorage.setItem('hackerNewsComponents', JSON.stringify(components))
     }
   }
-};
+}
